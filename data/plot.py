c901b27e4822b213bd138beaa5bffedac4d2e9ee
Russell ZHENG <russzheng@Russells-MacBook-Pro.local>
2017-04-06 18:36:50 -0400

    plot.py

--- data/plot.py
+++ data/plot.py
@@ -14,14 +14,16 @@ import seaborn as sns
 data = pds.read_csv('clean_noshow.csv')
 
 def main():
-	#sns.set_style("whitegrid")
-	#general_ratio()
-	#age()
-	#gapofDays()
-	sns.set_style("white")
-	#gender()
-	dayoftheWeek_reg()
-
+    sns.set_style("whitegrid")
+    #general_ratio()
+    #age()
+    #gapofDays()
+    #sns.set_style("white")
+    #gender()
+    #dayoftheWeek_reg()
+    timeofDay()
+    #timeofDay_lm()
+    
 def probStatusLR(dataset, group_by):
     df = pds.crosstab(index = dataset[group_by], columns = dataset.Status).reset_index()
     df['probNoShow'] = df[0] / (df[1] + df[0])
@@ -39,64 +41,101 @@ def probStatusBar(group_by):
     return pds.DataFrame(rows)
 
 def age():
-	sns.lmplot(data = probStatusLR(data, 'Age'), x = 'Age', y = 'probNoShow', fit_reg = True)
-	sns.plt.xlim(0, 100)
-	sns.plt.title('No-show ratio with respect to Age')
-	sns.plt.show()
+    sns.lmplot(data = probStatusLR(data, 'Age'), x = 'Age', y = 'probNoShow', fit_reg = True)
+    sns.plt.xlim(0, 100)
+    sns.plt.title('No-show ratio with respect to Age')
+    sns.plt.show()
 
 def gapofDays():
-	sns.lmplot(data = probStatusLR(data, 'GapofDays'), x = 'GapofDays', y = 'probNoShow', fit_reg = True)
-	sns.plt.title('No-show ratio with respect to GapofDays')
-	sns.plt.show()
+    sns.lmplot(data = probStatusLR(data, 'GapofDays'), x = 'GapofDays', y = 'probNoShow', fit_reg = True)
+    sns.plt.title('No-show ratio with respect to GapofDays')
+    sns.plt.show()
 
 def gender():
-	sns.barplot(data = probStatusBar(['Gender']),
+    sns.barplot(data = probStatusBar(['Gender']),
             x = 'x', y = 'Ratio', hue = 'Hue', palette = 'Set1')
-	sns.plt.title('No-show Ratio with respect to gender')
-	sns.plt.xlabel('Gender')
-	sns.plt.ylabel('No-show ratio')
-	sns.plt.show()
+    sns.plt.title('No-show Ratio with respect to gender')
+    sns.plt.xlabel('Gender')
+    sns.plt.ylabel('No-show ratio')
+    sns.plt.show()
 
 def dayoftheWeek():
-	sns.barplot(data = probStatusBar(['DayOfTheWeek']),
+    sns.barplot(data = probStatusBar(['DayOfTheWeek']),
             x = 'x', y = 'Ratio', hue = 'Hue', palette = 'Set1')
-	sns.plt.title('No-show Ratio with respect to Day of the Week')
-	sns.plt.xlabel('Day of the Week')
-	sns.plt.ylabel('No-show ratio')
-	sns.plt.show()
+    sns.plt.title('No-show Ratio with respect to Day of the Week')
+    sns.plt.xlabel('Day of the Week')
+    sns.plt.ylabel('No-show ratio')
+    sns.plt.show()
 
 def dayoftheWeek_reg():
-	add_column()
-	sns.barplot(data = probStatusBar(['DayOfTheWeekReg']),
+    add_column()
+    sns.barplot(data = probStatusBar(['DayOfTheWeekReg']),
             x = 'x', y = 'Ratio', hue = 'Hue', palette = 'Set1')
-	sns.plt.title('No-show Ratio with respect to Day of the Week (Registration)')
-	sns.plt.xlabel('Day of the Week')
-	sns.plt.ylabel('No-show ratio')
-	sns.plt.show()
+    sns.plt.title('No-show Ratio with respect to Day of the Week (Registration)')
+    sns.plt.xlabel('Day of the Week')
+    sns.plt.ylabel('No-show ratio')
+    sns.plt.show()
+    data.drop('DayOfTheWeekReg', 1)
+
+def timeofDay():
+    add_column_time()
+    sns.barplot(data = probStatusBar(['TimeOfDay']),
+            x = 'x', y = 'Ratio', hue = 'Hue', palette = 'Set1')
+    sns.plt.title('No-show Ratio with respect to Time of the Day')
+    sns.plt.xlabel('Time of the Day')
+    sns.plt.ylabel('No-show ratio')
+    sns.plt.show()
+    data.drop('TimeOfDay', 1)
+
+def timeofDay_lm():
+    add_column_time()
+    sns.lmplot(data = probStatusLR(data, 'TimeOfDay'), x = 'TimeOfDay', y = 'probNoShow', fit_reg = True)
+    sns.plt.xlim(5, 25)
+    sns.plt.ylim(0.2, 0.4)
+    sns.plt.title('No-show ratio with respect to TimeOfDay')
+    sns.plt.show()
 
 def add_column():
-	#Add columns day of week dfor registration
-	li = []
-	for index, row in data.iterrows():
-		temp = (row['DayOfTheWeek'] - row['GapofDays']) % 7
-		if temp == 0:
-			temp = 7
-		li.append(temp)
-	data.insert(4, 'DayOfTheWeekReg', li)
-	#print(data['DayOfTheWeekReg'].unique())
-	
-def general_ratio():
-	#first calculate the general no-show ratio
-	show_up = 0
-	no_show = 0
-	for item in data['Status']:
-		if item:
-			show_up += 1
-		else:
-			no_show += 1
+    #Add columns day of week dfor registration
+    li = []
+    for index, row in data.iterrows():
+        temp = (row['DayOfTheWeek'] - row['GapofDays']) % 7
+        if temp == 0:
+            temp = 7
+        li.append(temp)
+    data.insert(4, 'DayOfTheWeekReg', li)
+    #print(data['DayOfTheWeekReg'].unique())
 
-	print ('No-show ratio is ', (no_show / (show_up + no_show)))
+def add_column_time():
+    #Add columns time of the day of registration
+    count = 0
+    li = []
+    for index, row in data.iterrows():
+        li.append(row['AppointmentRegistration'][-5:-3])
+        if row['AppointmentRegistration'][-5:-3] == '22': #'05':
+            count += 1
+    data.insert(4, 'TimeOfDay', li)
+    #print(type(row['AppointmentRegistration'][-5:-3]))
+    print(count)
+    for index, row in data.iterrows():
+        if row['TimeOfDay'] == '05' or row['TimeOfDay'] == '22':
+            data.drop(index, inplace=True)
+    data['TimeOfDay'] = data['TimeOfDay'].map({'06': 6, '07': 7, '08': 8, '09': 9,
+    '10': 10, '11': 11, '12': 12, '13': 13, '14': 14, '15': 15, '16': 16, '17': 17,
+    '18': 18, '19': 19, '20': 20, '21': 21, '22': 22})
+
+def general_ratio():
+    #first calculate the general no-show ratio
+    show_up = 0
+    no_show = 0
+    for item in data['Status']:
+        if item:
+            show_up += 1
+        else:
+            no_show += 1
+
+    print ('No-show ratio is ', (no_show / (show_up + no_show)))
 
 if __name__ == '__main__':
-	main()
+    main()
 
