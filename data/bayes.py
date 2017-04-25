d09127b798d69a37f033702d86da7543f01fc49d
Russell ZHENG <russzheng@russells-mbp.devices.brown.edu>
2017-04-24 21:27:31 -0400

    more statistical models

--- data/bayes.py
+++ data/bayes.py
@@ -4,6 +4,9 @@ import math
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVR
 
 #Age,Gender,AppointmentRegistration,AppointmentData,GapofDays,DayOfTheWeek,Status,Diabetes,
 #Alchoholism,Hypertension,Handicap,Smokes,Scholarship,Tuberculosis,Sms_Reminder
@@ -24,10 +27,18 @@ for sample_size in [150000, 170000, 200000, 210000, 230000, 250000, 270000, 2900
 	features_test = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
 	                         'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[sample_size:]
 	labels_test = df.Status[sample_size:]
+
+	lr = LogisticRegression().fit(features_train, labels_train)
+	print('Accuracy for Logistic Regression:', accuracy_score(labels_test, lr.predict(features_test), 2))
 	
 	gnb =  GaussianNB().fit(features_train, labels_train)
 	print('Accuracy for GaussianNB:', accuracy_score(labels_test, gnb.predict(features_test), 2))
 
 	clf =  MultinomialNB().fit(features_train, labels_train)
 	print('Accuracy for MultinomialNB:', accuracy_score(labels_test, clf.predict(features_test), 2))
+
+	linear_svc = LinearSVC().fit(features_train, labels_train)
+	print('Accuracy for linear SVC:', accuracy_score(labels_test, linear_svc.predict(features_test), 2))
+
+	print('\n')
 
