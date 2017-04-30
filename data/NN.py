8dfb99740aa94c90537c3c5de7e362eff3821ceb
Russell ZHENG <russzheng@russells-mbp.devices.brown.edu>
2017-04-29 23:17:51 -0400

    Neural Network

---
+++ data/NN.py
@@ -0,0 +1,55 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+# load dataset
+df = pandas.read_csv('for_review.csv')
+df = df[df.DayOfTheWeek != 7]
+sample_size = 10000
+# split into input (X) and output (Y) variables
+features_train = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
+                             'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[:sample_size]
+features_train.to_csv('temp.csv', header=False, index=False)
+features_train = pandas.read_csv('temp.csv')
+features_train = features_train.values
+features_train = features_train.astype('float32')
+
+labels_train = df.Status[:sample_size]
+labels_train.to_csv('temp_2.csv', header=False, index=False)
+labels_train = pandas.read_csv('temp_2.csv')
+
+labels_train = numpy.array(labels_train)
+labels_train = numpy.ravel(labels_train)
+
+# baseline model
+def create_baseline():
+	# create model
+	model = Sequential()
+	model.add(Dense(12, input_dim=10, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(6, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=10, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, features_train, labels_train, cv=kfold)
+print('Training size:', sample_size)
+print("Standardized: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
--- data/bayes.py
+++ data/bayes.py
@@ -9,6 +9,12 @@ from sklearn.svm import LinearSVC
 from sklearn.svm import LinearSVR
 from sklearn.svm import SVR
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.svm
+import optunity
+import optunity.metrics
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.multiclass import OneVsRestClassifier
 
 #Age,Gender,AppointmentRegistration,AppointmentData,GapofDays,DayOfTheWeek,Status,Diabetes,
 #Alchoholism,Hypertension,Handicap,Smokes,Scholarship,Tuberculosis,Sms_Reminder
@@ -18,29 +24,53 @@ df = df.drop('GapofDays', 1)
 df = df.drop('Gender', 1)
 df = df.drop('AppointmentRegistration', 1)
 df = df.drop('AppointmentData', 1)
+df = df[df.DayOfTheWeek != 7]
 
 for sample_size in [150000, 170000, 200000, 210000, 230000, 250000, 270000, 290000]:
-	print('Sample size is: ', sample_size)
+    print('Sample size is: ', sample_size)
 
-	features_train = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
-	                         'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[:sample_size]
-	labels_train = df.Status[:sample_size]
+    features_train = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
+                             'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[:sample_size]
+    labels_train = df.Status[:sample_size]
 
-	features_test = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
-	                         'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[sample_size:]
-	labels_test = df.Status[sample_size:]
+    features_test = df[['Age', 'DayOfTheWeek', 'Diabetes', 'Hypertension', 'Tuberculosis', 'Smokes',
+                             'Handicap', 'Alchoholism', 'Scholarship', 'Sms_Reminder']].iloc[sample_size:]
+    labels_test = df.Status[sample_size:]
 
-	lr = LogisticRegression().fit(features_train, labels_train)
-	print('Accuracy for Logistic Regression:', accuracy_score(labels_test, lr.predict(features_test), 2))
-	
-	gnb =  GaussianNB().fit(features_train, labels_train)
-	print('Accuracy for GaussianNB:', accuracy_score(labels_test, gnb.predict(features_test), 2))
+    features_train = features_train.astype('float32')
+    features_test = features_test.astype('float32')
 
-	clf =  MultinomialNB().fit(features_train, labels_train)
-	print('Accuracy for MultinomialNB:', accuracy_score(labels_test, clf.predict(features_test), 2))
+    lr = LogisticRegression().fit(features_train, labels_train)
+    print('Accuracy for Logistic Regression:', accuracy_score(labels_test, lr.predict(features_test)))
+    print(len(lr.predict(features_test)) - np.count_nonzero(lr.predict(features_test)))
 
-	linear_svc = LinearSVR().fit(features_train, labels_train)
-	print('Accuracy for linear SVC:', accuracy_score(labels_test, linear_svc.predict(features_test), 2))
+    gnb =  GaussianNB().fit(features_train, labels_train)
+    print('Accuracy for GaussianNB:', accuracy_score(labels_test, gnb.predict(features_test), 2))
+    print(len(gnb.predict(features_test)) - np.count_nonzero(gnb.predict(features_test)))
 
-	print('\n')
+    clf =  MultinomialNB().fit(features_train, labels_train)
+    print('Accuracy for MultinomialNB:', accuracy_score(labels_test, clf.predict(features_test), 2))
+    print(len(clf.predict(features_test)) - np.count_nonzero(clf.predict(features_test)))
+    
+    linear_svc = LinearSVC().fit(features_train, labels_train)
+    print('Accuracy for linear SVC:', accuracy_score(labels_test, linear_svc.predict(features_test), 2))
+    print(len(linear_svc.predict(features_test)) - np.count_nonzero(linear_svc.predict(features_test)))
 
+    rf = RandomForestClassifier(n_jobs=2)
+    rf.fit(features_train, labels_train)
+    print('Accuracy for Random Forest:', accuracy_score(labels_test, rf.predict(features_test), 2))
+    print(len(rf.predict(features_test)) - np.count_nonzero(rf.predict(features_test)))
+
+    '''svc = SVC().fit(features_train, labels_train)
+    print('Accuracy for SVC:', accuracy_score(labels_test, svc.predict(features_test), 2))
+    print(len(linear_svc.predict(features_test)) - np.count_nonzero(svc.predict(features_test)))'''
+
+    n_estimators = 10
+    bsvc = OneVsRestClassifier(BaggingClassifier(SVC(kernel='linear'), max_samples=1.0 / n_estimators, n_estimators=n_estimators))
+    bsvc.fit(features_train, labels_train)
+    print('Accuracy for Bagging SVC:', accuracy_score(labels_test, bsvc.predict(features_test), 2))
+
+
+    print('\n')
+
+df.to_csv('for_review.csv', mode = 'w', index=False)
